851319e46a14e5b79be231fce359bc945ef85c7a
Nick <solitarycipher@gmail.com>
2018-01-26 14:02:29 -0500

    edit rofi config

--- bin/start_rofi.sh
+++ bin/start_rofi.sh
@@ -5,12 +5,11 @@ TERMINAL=urxvt
 rofi -combi-modi window,drun,ssh \
      -show combi \
      -modi combi \
-     -lines 5 \
-     -width 100 \
-     -padding 200 \
-     -fullscreen -opacity '85' \
-     -font 'Fira Mono 30' \
-     -separator-style 'none'  \
      -terminal $TERMINAL \
      -ssh-terminal $TERMINAL
-
+     -separator-style 'none'  \
+     #-font 'Fira Mono 30' \
+     #-fullscreen -opacity '85' \
+     #-lines 5 \
+     #-width 100 \
+     #-padding 200 \
